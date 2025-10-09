30f78c3ad373f512f7e2335085e1e4122393a733
liuchangqing <clooker@163.com>
2025-10-09 16:32:02 +0800

    fix(core): 命令分类

--- cli/commands.go
+++ cli/commands.go
@@ -14,14 +14,13 @@ func (c *CLI) setupCommands() *cobra.Command {
   rootCmd := &cobra.Command{
     Use:     "vigil",
     Short:   "Process Management Program",
-    Long:    "Vigil - A powerful proc management and monitoring tool",
+    Long:    "Vigil - A powerful process management and monitoring tool",
     Version: "1.0.0",
   }
 
   // Global flags
   rootCmd.PersistentFlags().StringVarP(&apiHost, "host", "H", "http://localhost:8080", "API server host address")
-  // namespace不再作为全局变量定义，而是在各命令中处理
-
+  
   // Override PreRun to create client with the provided host
   rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
     // Only create a new client if we're not in the root command
@@ -31,10 +30,52 @@ func (c *CLI) setupCommands() *cobra.Command {
     return nil
   }
 
-  // Scan command
+  // Add subcommands
+  procCmd := c.setupProcCommands()
+  rootCmd.AddCommand(procCmd)
+  
+  resourceCmd := c.setupResourceCommands()
+  rootCmd.AddCommand(resourceCmd)
+  
+  configCmd := c.setupConfigCommands()
+  rootCmd.AddCommand(configCmd)
+  
+  execCmd := c.setupExecCommand()
+  rootCmd.AddCommand(execCmd)
+
+  return rootCmd
+}
+
+// setupProcCommands 设置所有进程管理相关的命令
+func (c *CLI) setupProcCommands() *cobra.Command {
+  // Proc command - 作为父命令来组织所有进程相关的子命令
+  procCmd := &cobra.Command{
+    Use:   "proc",
+    Short: "Process management operations",
+    Long:  "Manage and monitor processes with various operations",
+  }
+
+  // 添加各个子命令
+  procCmd.AddCommand(c.setupScanCommand())
+  procCmd.AddCommand(c.setupCreateCommand())
+  procCmd.AddCommand(c.setupStartCommand())
+  procCmd.AddCommand(c.setupStopCommand())
+  procCmd.AddCommand(c.setupRestartCommand())
+  procCmd.AddCommand(c.setupDeleteCommand())
+  procCmd.AddCommand(c.setupListCommand())
+  procCmd.AddCommand(c.setupStatusCommand())
+  procCmd.AddCommand(c.setupEditCommand())
+  procCmd.AddCommand(c.setupGetCommand())
+
+  return procCmd
+}
+
+// setupScanCommand 设置scan命令
+func (c *CLI) setupScanCommand() *cobra.Command {
   var query string
   var registerAfterScan bool
   var scanNamespace string
+  
   scanCmd := &cobra.Command{
     Use:   "scan",
     Short: "Scan processes",
@@ -45,18 +86,23 @@ func (c *CLI) setupCommands() *cobra.Command {
     },
   }
   scanCmd.Flags().StringVarP(&query, "query", "q", "", "Search query string or regex")
-  scanCmd.Flags().BoolVarP(&registerAfterScan, "register", "r", false, "Register a proc after scanning")
+  scanCmd.Flags().BoolVarP(&registerAfterScan, "register", "r", false, "Register a process after scanning")
   scanCmd.Flags().StringVarP(&scanNamespace, "namespace", "n", "default", "Process namespace")
   scanCmd.MarkFlagRequired("query")
+  
+  return scanCmd
+}
 
-  // Manage command
+// setupCreateCommand 设置create命令
+func (c *CLI) setupCreateCommand() *cobra.Command {
   var processName string
   var commandPath string
   var createNamespace string
+  
   createCmd := &cobra.Command{
     Use:   "create [name]",
-    Short: "Manage proc",
-    Long:  "Manage an existing proc or create a new managed proc",
+    Short: "Create process",
+    Long:  "Create a new managed process",
     Args:  cobra.MaximumNArgs(1),
     RunE: func(cmd *cobra.Command, args []string) error {
       namespaceFlag, _ := cmd.Flags().GetString("namespace")
@@ -70,13 +116,18 @@ func (c *CLI) setupCommands() *cobra.Command {
   createCmd.Flags().StringVarP(&processName, "name", "N", "", "Process name (alternative to positional argument)")
   createCmd.Flags().StringVarP(&commandPath, "command", "c", "", "Command path")
   createCmd.Flags().StringVarP(&createNamespace, "namespace", "n", "default", "Process namespace")
+  
+  return createCmd
+}
 
-  // Start command
+// setupStartCommand 设置start命令
+func (c *CLI) setupStartCommand() *cobra.Command {
   var startNamespace string
+  
   startCmd := &cobra.Command{
     Use:   "start [name]",
-    Short: "Start proc",
-    Long:  "Start a managed proc. If no name is provided, an interactive selection will be shown.",
+    Short: "Start process",
+    Long:  "Start a managed process. If no name is provided, an interactive selection will be shown.",
     Args:  cobra.MaximumNArgs(1),
     RunE: func(cmd *cobra.Command, args []string) error {
       namespaceFlag, _ := cmd.Flags().GetString("namespace")
@@ -90,13 +141,18 @@ func (c *CLI) setupCommands() *cobra.Command {
     },
   }
   startCmd.Flags().StringVarP(&startNamespace, "namespace", "n", "default", "Process namespace")
+  
+  return startCmd
+}
 
-  // Stop command
+// setupStopCommand 设置stop命令
+func (c *CLI) setupStopCommand() *cobra.Command {
   var stopNamespace string
+  
   stopCmd := &cobra.Command{
     Use:   "stop [name]",
-    Short: "Stop proc",
-    Long:  "Stop a managed proc. If no name is provided, an interactive selection will be shown.",
+    Short: "Stop process",
+    Long:  "Stop a managed process. If no name is provided, an interactive selection will be shown.",
     Args:  cobra.MaximumNArgs(1),
     RunE: func(cmd *cobra.Command, args []string) error {
       namespaceFlag, _ := cmd.Flags().GetString("namespace")
@@ -110,13 +166,18 @@ func (c *CLI) setupCommands() *cobra.Command {
     },
   }
   stopCmd.Flags().StringVarP(&stopNamespace, "namespace", "n", "default", "Process namespace")
+  
+  return stopCmd
+}
 
-  // Restart command
+// setupRestartCommand 设置restart命令
+func (c *CLI) setupRestartCommand() *cobra.Command {
   var restartNamespace string
+  
   restartCmd := &cobra.Command{
     Use:   "restart [name]",
-    Short: "Restart proc",
-    Long:  "Restart a managed proc. If no name is provided, an interactive selection will be shown.",
+    Short: "Restart process",
+    Long:  "Restart a managed process. If no name is provided, an interactive selection will be shown.",
     Args:  cobra.MaximumNArgs(1),
     RunE: func(cmd *cobra.Command, args []string) error {
       namespaceFlag, _ := cmd.Flags().GetString("namespace")
@@ -130,13 +191,18 @@ func (c *CLI) setupCommands() *cobra.Command {
     },
   }
   restartCmd.Flags().StringVarP(&restartNamespace, "namespace", "n", "default", "Process namespace")
+  
+  return restartCmd
+}
 
-  // Delete command
+// setupDeleteCommand 设置delete命令
+func (c *CLI) setupDeleteCommand() *cobra.Command {
   var deleteNamespace string
+  
   deleteCmd := &cobra.Command{
     Use:   "delete [name]",
-    Short: "Delete a managed proc",
-    Long:  "Delete a proc from the managed list. If the proc is running, it will be stopped first. If no name is provided, an interactive selection will be shown.",
+    Short: "Delete a managed process",
+    Long:  "Delete a process from the managed list. If the process is running, it will be stopped first. If no name is provided, an interactive selection will be shown.",
     Args:  cobra.MaximumNArgs(1),
     RunE: func(cmd *cobra.Command, args []string) error {
       namespaceFlag, _ := cmd.Flags().GetString("namespace")
@@ -150,9 +216,14 @@ func (c *CLI) setupCommands() *cobra.Command {
     },
   }
   deleteCmd.Flags().StringVarP(&deleteNamespace, "namespace", "n", "default", "Process namespace")
+  
+  return deleteCmd
+}
 
-  // List command
+// setupListCommand 设置list命令
+func (c *CLI) setupListCommand() *cobra.Command {
   var listNamespace string
+  
   listCmd := &cobra.Command{
     Use:   "list",
     Short: "List processes",
@@ -163,13 +234,18 @@ func (c *CLI) setupCommands() *cobra.Command {
     },
   }
   listCmd.Flags().StringVarP(&listNamespace, "namespace", "n", "default", "Process namespace")
+  
+  return listCmd
+}
 
-  // Status command
+// setupStatusCommand 设置status命令
+func (c *CLI) setupStatusCommand() *cobra.Command {
   var statusNamespace string
+  
   statusCmd := &cobra.Command{
     Use:   "status [name]",
-    Short: "Check proc status",
-    Long:  "Check the status of a managed proc",
+    Short: "Check process status",
+    Long:  "Check the status of a managed process",
     Args:  cobra.ExactArgs(1),
     RunE: func(cmd *cobra.Command, args []string) error {
       namespaceFlag, _ := cmd.Flags().GetString("namespace")
@@ -177,48 +253,44 @@ func (c *CLI) setupCommands() *cobra.Command {
     },
   }
   statusCmd.Flags().StringVarP(&statusNamespace, "namespace", "n", "default", "Process namespace")
+  
+  return statusCmd
+}
 
-  // Resource commands
-  systemResourceCmd := &cobra.Command{
-    Use:   "system-resources",
-    Short: "Get system resources",
-    Long:  "Get system resource usage information",
-    RunE: func(cmd *cobra.Command, args []string) error {
-      return c.handleSystemResources()
-    },
-  }
-
-  processResourceCmd := &cobra.Command{
-    Use:   "process-resources [pid]",
-    Short: "Get process resources",
-    Long:  "Get resource usage information for a specific process",
+// setupEditCommand 设置edit命令
+func (c *CLI) setupEditCommand() *cobra.Command {
+  var editNamespace string
+  
+  editCmd := &cobra.Command{
+    Use:   "edit [name]",
+    Short: "Edit process definition",
+    Long:  "Edit the definition of a managed process using vim editor. If no name is provided, an interactive selection will be shown.",
     Args:  cobra.MaximumNArgs(1),
     RunE: func(cmd *cobra.Command, args []string) error {
-      pid := 0
-      if len(args) > 0 {
-        fmt.Sscanf(args[0], "%d", &pid)
+      namespaceFlag, _ := cmd.Flags().GetString("namespace")
+
+      // 如果没有提供参数，使用交互式选择
+      if len(args) == 0 {
+        return c.handleEditInteractive(namespaceFlag)
       }
-      return c.handleProcessResources(pid)
+
+      return c.handleEdit(args[0], namespaceFlag)
     },
   }
+  editCmd.Flags().StringVarP(&editNamespace, "namespace", "n", "default", "Process namespace")
+  
+  return editCmd
+}
 
-  // Config commands
-  getConfigCmd := &cobra.Command{
-    Use:   "get-config",
-    Short: "Get configuration",
-    Long:  "Get the current configuration",
-    RunE: func(cmd *cobra.Command, args []string) error {
-      return c.handleGetConfig()
-    },
-  }
-
-  // Get command - 新增支持YAML格式输出的get命令
+// setupGetCommand 设置get命令
+func (c *CLI) setupGetCommand() *cobra.Command {
   var getFormat string
   var getNamespace string
+  
   getCmd := &cobra.Command{
     Use:   "get [name]",
-    Short: "Get proc details",
-    Long:  "Get detailed information about a managed proc. If no name is provided, an interactive selection will be shown.",
+    Short: "Get process details",
+    Long:  "Get detailed information about a managed process. If no name is provided, an interactive selection will be shown.",
     Args:  cobra.MaximumNArgs(1),
     RunE: func(cmd *cobra.Command, args []string) error {
       namespaceFlag, _ := cmd.Flags().GetString("namespace")
@@ -234,32 +306,83 @@ func (c *CLI) setupCommands() *cobra.Command {
   }
   getCmd.Flags().StringVarP(&getFormat, "format", "f", "yaml", "Output format (yaml|text)")
   getCmd.Flags().StringVarP(&getNamespace, "namespace", "n", "default", "Process namespace")
+  
+  return getCmd
+}
 
-  // Edit command
-  var editNamespace string
-  editCmd := &cobra.Command{
-    Use:   "edit [name]",
-    Short: "Edit proc definition",
-    Long:  "Edit the definition of a managed proc using vim editor. If no name is provided, an interactive selection will be shown.",
-    Args:  cobra.MaximumNArgs(1),
+// setupResourceCommands 设置资源相关命令
+func (c *CLI) setupResourceCommands() *cobra.Command {
+  resourceCmd := &cobra.Command{
+    Use:   "resources",
+    Short: "Resource management operations",
+    Long:  "View and manage system and process resources",
+  }
+
+  // System resources command
+  systemResourceCmd := &cobra.Command{
+    Use:   "system",
+    Short: "Get system resources",
+    Long:  "Get system resource usage information",
     RunE: func(cmd *cobra.Command, args []string) error {
-      namespaceFlag, _ := cmd.Flags().GetString("namespace")
-
-      // 如果没有提供参数，使用交互式选择
-      if len(args) == 0 {
-        return c.handleEditInteractive(namespaceFlag)
-      }
-
-      return c.handleEdit(args[0], namespaceFlag)
+      return c.handleSystemResources()
     },
   }
-  editCmd.Flags().StringVarP(&editNamespace, "namespace", "n", "default", "Process namespace")
 
-  // Exec command - 新增执行脚本命令
+  // Process resources command
+  processResourceCmd := &cobra.Command{
+    Use:   "process [pid]",
+    Short: "Get process resources",
+    Long:  "Get resource usage information for a specific process",
+    Args:  cobra.MaximumNArgs(1),
+    RunE: func(cmd *cobra.Command, args []string) error {
+      pid := 0
+      if len(args) > 0 {
+        fmt.Sscanf(args[0], "%d", &pid)
+      }
+      return c.handleProcessResources(pid)
+    },
+  }
+
+  // Add subcommands to resources command
+  resourceCmd.AddCommand(systemResourceCmd)
+  resourceCmd.AddCommand(processResourceCmd)
+
+  return resourceCmd
+}
+
+// setupConfigCommands 设置配置相关命令
+func (c *CLI) setupConfigCommands() *cobra.Command {
+  // Config command - 作为父命令来组织所有配置相关的子命令
+  configCmd := &cobra.Command{
+    Use:   "config",
+    Short: "Configuration operations",
+    Long:  "View and manage system configuration",
+  }
+
+  // Get config command
+  getConfigCmd := &cobra.Command{
+    Use:   "get",
+    Short: "Get configuration",
+    Long:  "Get the current configuration",
+    RunE: func(cmd *cobra.Command, args []string) error {
+      return c.handleGetConfig()
+    },
+  }
+
+  // Add subcommands to config command
+  configCmd.AddCommand(getConfigCmd)
+
+  // 可以根据需要添加更多配置相关的命令
+
+  return configCmd
+}
+
+// setupExecCommand 设置exec命令
+func (c *CLI) setupExecCommand() *cobra.Command {
   var isFile bool
   var envVars []string
   var outputFile string
-  // 注意：原有的-f参数已经被用作--file标志，所以使用-r作为--result的短选项
+  
   execCmd := &cobra.Command{
     Use:   "exec [command/script]",
     Short: "Execute a command or script",
@@ -272,22 +395,6 @@ func (c *CLI) setupCommands() *cobra.Command {
   execCmd.Flags().BoolVarP(&isFile, "file", "f", false, "Treat the argument as a script file path")
   execCmd.Flags().StringArrayVarP(&envVars, "env", "e", []string{}, "Environment variables to set (format: KEY=VALUE)")
   execCmd.Flags().StringVarP(&outputFile, "result", "r", "", "Output result to file instead of console")
-
-  // Add all commands to root command
-  rootCmd.AddCommand(scanCmd)
-  rootCmd.AddCommand(createCmd)
-  rootCmd.AddCommand(startCmd)
-  rootCmd.AddCommand(stopCmd)
-  rootCmd.AddCommand(restartCmd)
-  rootCmd.AddCommand(deleteCmd)
-  rootCmd.AddCommand(listCmd)
-  rootCmd.AddCommand(statusCmd)
-  rootCmd.AddCommand(systemResourceCmd)
-  rootCmd.AddCommand(processResourceCmd)
-  rootCmd.AddCommand(getConfigCmd)
-  rootCmd.AddCommand(getCmd)
-  rootCmd.AddCommand(editCmd)
-  rootCmd.AddCommand(execCmd)
-
-  return rootCmd
+  
+  return execCmd
 }
